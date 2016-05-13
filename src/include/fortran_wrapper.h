e68217e36e9803cdde8945fbed420767f3134b09
T.Q <pasuka@foxmail.com>
2016-05-13 14:46:10 +0800

    Continue fortran wrapper...

---
+++ src/include/fortran_wrapper.h
@@ -0,0 +1,37 @@
+#ifndef FORTRAN_WRAPPER_H
+#define FORTRAN_WRAPPER_H
+
+namespace cafea
+{
+#ifdef __cplusplus
+extern "C" {
+#endif
+const int LEN_ARRAY_{16};
+const int MAX_NODES_PER_ELEM_{27};
+
+struct node_f {
+	int id_{-1};
+	int csys_{0};
+	int boundary_[LEN_ARRAY_]={0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	REAL4 xyz_[3]={0E0, 0E0, 0E0};
+	REAL4 rot_[3]={1.81E2, 1.81E2, 1.81E2};
+};
+
+struct elem_f {
+	int id_{-1};
+	int etype_{0};
+	int prop_[2]={0, 0};
+	int opt_[LEN_ARRAY_]={0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int node_list_[MAX_NODES_PER_ELEM_]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+};
+
+struct matl_f {
+	int id_{-1};
+	int sect_{0};
+	REAL4 val_[LEN_ARRAY_]={0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0,0E0};
+};
+#ifdef __cplusplus
+}
+#endif
+}
+#endif
