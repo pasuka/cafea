b363f1d58f4e9b218a1a2a20a8db67494cc49523
T.Q <pasuka@foxmail.com>
2017-04-22 03:42:12 +0800

    *Working test case of CDB Reader.

--- test/basic/f01.cc
+++ test/basic/f01.cc
@@ -17,7 +17,7 @@ std::map<std::string, std::map<std::string, size_t>> examples {
     {"./ansys/pipe_demo_03.cdb", {{"node", 53}, {"element", 52}, {"material", 2}, {"real_constant", 7}, {"BC", 19}}},
     {"./ansys/nrc1677_1_1.cdb", {{"node", 11}, {"element", 19}, {"material", 1}, {"real_constant", 11}, {"BC", 2}}},
     {"./ansys/nrc1677_1_2.cdb", {{"node", 18}, {"element", 32}, {"material", 1}, {"real_constant", 15}, {"BC", 4}}},
-    {"./ansys/nrc1677_1_3.cdb", {{"node", 35}, {"element", 35}, {"material", 6}, {"real_constant", 2}, {"BC", 16}}},
+    {"./ansys/nrc1677_1_3.cdb", {{"node", 35}, {"element", 35}, {"material", 2}, {"real_constant", 6}, {"BC", 16}}},
     {"./ansys/nrc1677_1_4.cdb", {{"node", 115}, {"element", 123}, {"material", 1}, {"real_constant", 21}, {"BC", 26}}},
     {"./ansys/nrc1677_1_5.cdb", {{"node", 49}, {"element", 52}, {"material", 3}, {"real_constant", 23}, {"BC", 8}}},
     {"./ansys/nrc1677_1_6.cdb", {{"node", 68}, {"element", 88}, {"material", 1}, {"real_constant", 29}, {"BC", 19}}},
@@ -34,8 +34,10 @@ TEST_CASE("demo", "[CDBReader]") {
         for (const auto &x: examples) {
             fp.load_model(x.first);
             auto info = fp.get_info();
+            auto lib_ref = x.second;
             for (const auto &p: info) {
                 std::cout << p.first << " " << p.second << "\n";
+                REQUIRE(p.second == lib_ref[p.first]);
             }
             fp.clean_model();
         }
